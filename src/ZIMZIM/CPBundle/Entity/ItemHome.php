13cabe77a969d33867cb4b8dd19096edec25f830
zimzim62000 <zimzim62000@gmail.com>
2014-11-30 23:31:57 +0100

    add image itemhome ifx gitignore

--- src/ZIMZIM/CPBundle/Entity/ItemHome.php
+++ src/ZIMZIM/CPBundle/Entity/ItemHome.php
@@ -12,8 +12,10 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\InheritanceType("SINGLE_TABLE")
  * @ORM\DiscriminatorColumn(name="discr", type="string")
  * @ORM\DiscriminatorMap({"ItemHomeCategory" = "ItemHomeCategory", "ItemHomeProduct" = "ItemHomeProduct"})
+ * @ORM\HasLifecycleCallbacks
  *
  */
 abstract class ItemHome extends baseItemHome
 {
+
 }
